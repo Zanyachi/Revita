1ecbfd6f02b4c756395d74a6da93af4ad42780d7
Eugene <anyatagene@gmail.com>
2022-06-14 14:05:18 +0100

    I updated the css and added a javascript

---
+++ rev.js
@@ -0,0 +1,26 @@
+// the variables
+
+var today = new Date();
+var hour = today.getHours();
+var greeting = document.querySelector('#h3');
+
+// the greeting due to time
+ 
+
+if (hour > 12) {
+    greeting.innerHTML = "Good Afternoon!";
+}
+
+else if (hour > 18) {
+    greeting.innerHTML = "Good Evening!";
+}
+
+else if (hour > 0){
+    greeting.innerHTML = "Welcome!";
+}
+
+else {
+    greeting.innerHTML = "See you tomorrow";
+}
+
+console.log(hour)
